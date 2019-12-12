658e684af1624782ebd32078f145bae305512cbf
Siddharthaarora <sidaro@live.com>
2019-12-11 22:20:41 -0800

    algo to find all permutations of chars in a string using backtracking mechanish

--- private/dev/ChallengePy/hashtable.py
+++ private/dev/ChallengePy/hashtable.py
@@ -1,5 +1,7 @@
 # My implementation of hash table
 # There are two ways we can implmenent a hash table:
-#   1. Using an array of linked lists
-#   2. Using a binary search tree
+#   1. Using an array of linked lists - O(1) look up
+#   2. Using a binary search tree - O(log n) look up
+
+
 

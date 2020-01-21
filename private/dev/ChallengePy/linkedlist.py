8b902898f4b72300ea37450bafe0ecb40ea217ff
Siddharthaarora <sidaro@live.com>
2020-01-20 21:31:18 -0800

    added new function to partition a given linked list

--- private/dev/ChallengePy/linkedlist.py
+++ private/dev/ChallengePy/linkedlist.py
@@ -46,14 +46,13 @@ class LinkedList:
 
 def GenerateSampleLinkedList():
     l = LinkedList()
-    l.Insert(4)
+    l.Insert(0)
     l.Insert(3)
     l.Insert(1)
     l.Insert(9)
     l.Insert(5)
     l.Insert(6)
-    l.Insert(4)
-    l.Insert(2)
+    l.Insert(10)
     return l
 
 def PrintLinkedList(l):
@@ -78,9 +77,46 @@ def RemoveDuplicates(l):
         h = h.next
     print(map)
 
+# Partition; Write code to partition a linked list around a value x, such that all nodes less than x come
+# before all nodes greater than or equal tox. If x is contained within the list, the values of x only need
+# to be after the elements less than x (see below). The partition element x can appear anywhere in the
+# "right partition"; it does not need to appear between the left and right partitions.
+# EXAMPLE
+# input: 3 -> S -> 8 -> 5 -> 10 -> 2 -> 1 [partition = 5]
+# Output: 3 -> 1 -> 2 -> 5 -> 5 -> 10 -> 8 
+
+def PartitionLinkedList(l, x):
+    h = l.head
+    curr = h
+    prev = h
+    xnode = LinkedListNode()
+    xnode.value = x
+    i = 0
+    while(curr != None):
+        print(h.value, prev.value, curr.value)
+        if(curr.value < x):
+            if (curr == h):
+                curr = curr.next
+            else:
+                prev.next = curr.next
+                curr.next = h.next
+                h.next = curr
+                curr = prev.next
+        else:
+            if (xnode.next == None):
+                xnode.next = curr.next
+                curr = xnode
+                prev.next = xnode
+            prev = prev.next
+            curr = curr.next
+        i = i + 1
+        if (i == 7):
+            break
+
 # driver code
 l = GenerateSampleLinkedList()
 PrintLinkedList(l)
 #l.Reverse()
-RemoveDuplicates(l)
+#RemoveDuplicates(l)
+PartitionLinkedList(l,4)
 PrintLinkedList(l)
